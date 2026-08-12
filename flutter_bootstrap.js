c407a9a2e86267f1374af661db81ef027442cb33
polleen25-oss <polleen25@gmail.com>
2026-08-13 01:28:45 +0700

    Update new features

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2401244434" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3071713599" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
